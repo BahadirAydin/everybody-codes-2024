aa24cd90da53ad7652acd30088d2642789594954
BahadirAydin <bahadiraydin1923@gmail.com>
2025-07-29 22:18:54 +0300

    formatting

--- src/common/input_reader.cpp
+++ src/common/input_reader.cpp
@@ -5,14 +5,14 @@
 
 namespace utils {
 std::string read_file(const std::string &filepath) {
-  std::ifstream file(filepath);
-  if (!file.is_open()) {
-    std::cerr << "Error: Could not open file " << filepath << std::endl;
-    return "";
-  }
+    std::ifstream file(filepath);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << filepath << std::endl;
+        return "";
+    }
 
-  std::stringstream buffer;
-  buffer << file.rdbuf();
-  return buffer.str();
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
 }
 } // namespace utils
