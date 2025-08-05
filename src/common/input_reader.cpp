340d9d7fb338475338c1d48e86a91ac32f5ace76
BahadirAydin <bahadiraydin1923@gmail.com>
2025-08-05 21:38:42 +0300

    new helpers

--- src/common/input_reader.cpp
+++ src/common/input_reader.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 namespace utils {
 std::string read_file(const std::string &filepath) {
@@ -14,5 +15,34 @@ std::string read_file(const std::string &filepath) {
     std::stringstream buffer;
     buffer << file.rdbuf();
     return buffer.str();
+}
+
+std::vector<std::string> read_lines(const std::string &filepath) {
+    std::ifstream file(filepath);
+    std::vector<std::string> lines;
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << filepath << std::endl;
+        return lines;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+    }
+
+    return lines;
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> result;
+    std::stringstream ss(s);
+    std::string item;
+
+    while (getline(ss, item, delim)) {
+        result.push_back(item);
+    }
+
+    return result;
 }
 } // namespace utils
--- src/common/input_reader.hpp
+++ src/common/input_reader.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace utils {
 
 std::string read_file(const std::string &filepath);
+std::vector<std::string> read_lines(const std::string &filepath);
+std::vector<std::string> split(const std::string &s, char delim);
 
-}
+} // namespace utils
