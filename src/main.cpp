0cc2464a915870dd58fbdac21576911bd45c9338
BahadirAydin <bahadiraydin1923@gmail.com>
2025-08-05 21:39:19 +0300

    better quest running in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
+#include "i_quest.hpp"
 #include "quest01.hpp"
+#include "quest02.hpp"
+
+#include <memory>
+#include <vector>
 
 int main() {
-    quest_01 quest;
-    quest.run_with_timing();
+    std::vector<std::unique_ptr<i_quest>> quests;
+    quests.emplace_back(std::make_unique<quest_01>());
+    quests.emplace_back(std::make_unique<quest_02>());
+
+    for (const auto &quest : quests) {
+        quest->run_with_timing();
+    }
+
     return 0;
 }
